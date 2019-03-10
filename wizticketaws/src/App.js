f6933e82732263970166e13a9def124a200f5099
Martin <martingarciadelangel@me.com>
2019-03-09 18:32:04 -0600

    Added things up to this point, dont kill

--- wizticketaws/src/App.js
+++ wizticketaws/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { withAuthenticator,AmplifyTheme } from 'aws-amplify-react'
-import logo from './logo.svg';
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react'
+import { Auth, Hub } from 'aws-amplify'
 import './App.css';
 
 const theme = {
@@ -16,7 +16,7 @@ const theme = {
   sectionBody: {
     ...AmplifyTheme.sectionBody,
     padding: "5px"
-  }, 
+  },
   sectionHeader: {
     ...AmplifyTheme.sectionHeader,
     backgroundColor: "var(--squidInk)"
@@ -24,27 +24,45 @@ const theme = {
 }
 
 class App extends Component {
+  state = {
+    user: null
+  }
+
+  componentDidMount() {
+    this.getUserData()
+    Hub.listen('auth', this, 'onHubCapsule')
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser()
+    user ? this.setState({ user }) : this.setState({ user: null })
+  }
+
+  onHubCapsule = capsule => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log('signed in!')
+        this.getUserData()
+        break;
+      case "signUp":
+        console.log('signed up!')
+        break;
+      case "signOff":
+        console.log('signed out')
+        this.setState({ user: null })
+        break;
+      default:
+        console.log('not supported')
+        break;
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { user } = this.state
+    return !user ? (<Authenticator theme={theme} />) : <div>App</div>
+
   }
 }
 
-export default withAuthenticator(App,true);
-
+// export default withAuthenticator(App,true);
+export default App
