837fa6806806c77794481fa517c306030044996d
Martin <martingarciadelangel@me.com>
2019-03-18 19:49:05 -0600

    Added Stripe and a Lambda Function to process payments

--- wizticketaws/src/components/BuyTickets.js
+++ wizticketaws/src/components/BuyTickets.js
@@ -5,6 +5,9 @@ import StripeCheckout from 'react-stripe-checkout'
 /** Element UI */
 import {Notification,Message} from 'element-react'
 
+/** AWS Stuff */
+import {API} from 'aws-amplify'
+
 const stripeConfig = {
 	currency: "MXN",
 	publishableAPIKey: "pk_test_pUwXaYwXsdJM9MDQupyDvg6F"
@@ -12,9 +15,31 @@ const stripeConfig = {
 
 const BuyTickets = ({tickets, user}) => {
 
+	const handleCharge = async token => {
+		try{
+			const result = await API.post('ticketlambda','/charge',{
+				body: {
+					token,
+					charge: {
+						currency: stripeConfig.currency,
+						amount: tickets.reduce(function(a,b){return a.value + b.value})*100,
+						description: tickets.length+"x Tickets"
+					}
+				}
+			})
+			console.log({result})
+		}catch(err){
+			Notification({
+				title: "Error",
+				message: "Couldn't process your payment, please try again",
+				type: "error"
+			})
+		}
+	}
 
 		return (
 			<StripeCheckout 
+				token={handleCharge}
 				currency={stripeConfig.currency} 
 				stripeKey={stripeConfig.publishableAPIKey}
 				email={user.attributes.email}
