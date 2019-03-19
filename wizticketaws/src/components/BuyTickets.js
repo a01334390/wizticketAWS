f3273e579e39a1b12fe42a60717fa04e729faa21
Martin <martingarciadelangel@me.com>
2019-03-18 19:00:11 -0600

    Added Stripe Checkout and Moved the User Auth

--- wizticketaws/src/components/BuyTickets.js
+++ wizticketaws/src/components/BuyTickets.js
@@ -1,15 +1,31 @@
 import React from "react";
+/** Stripe Stuff */
+import StripeCheckout from 'react-stripe-checkout'
 
-class BuyTickets extends React.Component {
-	state = {
+/** Element UI */
+import {Notification,Message} from 'element-react'
 
-	}
+const stripeConfig = {
+	currency: "MXN",
+	publishableAPIKey: "pk_test_pUwXaYwXsdJM9MDQupyDvg6F"
+}
 
-	render () {
+const BuyTickets = ({tickets, user}) => {
+
+
 		return (
-			<>Buy Tickets</>
+			<StripeCheckout 
+				currency={stripeConfig.currency} 
+				stripeKey={stripeConfig.publishableAPIKey}
+				email={user.attributes.email}
+				name={tickets.length+"x Tickets"}
+				amount={tickets.reduce(function(a,b){return a.value + b.value})*100}
+				shippingAddress={false}
+				billingAddress={false}
+				locale="auto"
+				allowRememberMe={false}
+				/>
 		)
-	}
 }
 
 export default BuyTickets
--- wizticketaws/src/pages/TicketSelection.js
+++ wizticketaws/src/pages/TicketSelection.js
@@ -12,6 +12,10 @@ import { getWizEvent } from '../graphql/queries'
 import { S3Image } from 'aws-amplify-react'
 import BuyTickets from "../components/BuyTickets";
 
+/** User Context */
+import { UserContext } from '../App'
+
+
 class TicketSelection extends React.Component {
 	constructor(props) {
 		super(props)
@@ -73,89 +77,108 @@ class TicketSelection extends React.Component {
 	render() {
 		const { event, isLoading, selectedTicket } = this.state
 
-		return isLoading ? (<Loading fullscreen={true} />) : (
-			<>
-				{/** Back Button */}
-				<Link className="link" to={`/place/${event.place.id}`}>
-					Back to Place List
-        	</Link>
-				<Card
-					className="box-card"
-					bodyStyle={{ minWidth: "200px", alignContent: "center" }}
-					header={
-						<div className="clearfix">
-							<span style={{ "lineHeight": "12px" }}> <h1>Select your tickets</h1></span>
-						</div>
-					}>
-					<S3Image
-						imgKey={event.pictures.key}
-						theme={{
-							photoImg: { maxWidth: "100%", maxHeight: "100%" }
-						}}
-					/>
-					<h1 className="mb-mr pt-2">{event.name}</h1>
-					<h2 className="mb-mr">{event.description}</h2>
-					<Icon name="date" className="date" />{event.validUntil}
-					<div>
-						<span className="pt-1">By: {event.owner}</span>
-					</div>
-					<Layout.Row style={{ marginTop: "1%" }}>
-						<Layout.Col span="16">
-							<Transfer
-								filterable
-								filterMethod={this._filterMethod}
-								filterPlaceholder="Categorias"
-								value={selectedTicket}
-								onChange={this._handleChange}
-								data={this.state.data}
-								titles={['Disponibles', 'Seleccionados']}
-
-								footerFormat={{
-									//eslint-disable-next-line
-									noChecked: '${total}',
-									//eslint-disable-next-line
-									hasChecked: '${checked}/${total}'
-								}}>
-
-							</Transfer>
-						</Layout.Col>
-						<Layout.Col span="8">
-							<Card style={{ marginTop: "1%" }}>
+		return (
+			
+			<UserContext.Consumer>
+				{({user}) => {
+					
+					return isLoading ? (<Loading fullscreen={true} />) : (
+						<>
+							{/** Back Button */}
+							<Link className="link" to={`/place/${event.place.id}`}>
+								Back to Place List
+						</Link>
+							<Card
+								className="box-card"
+								bodyStyle={{ minWidth: "200px", alignContent: "center" }}
+								header={
+									<div className="clearfix">
+										<span style={{ "lineHeight": "12px" }}> <h1>Select your tickets</h1></span>
+									</div>
+								}>
+								<S3Image
+									imgKey={event.pictures.key}
+									theme={{
+										photoImg: { maxWidth: "100%", maxHeight: "100%" }
+									}}
+								/>
 								<Layout.Row>
-									<Layout.Col span="24">
-										{selectedTicket.length === 0 ? (
-											<h3>No Tickets Selected</h3>
-										) : (<>
-											<h3>{selectedTicket.length} x Tickets Selected</h3>
-
-											<div>
-												{this.state.seats.map(seats => (
-													<>
-														<Card style={{marginTop: "1%", marginBottom: "1%"}}>
-															<h2>{seats.category}</h2>
-															<span>Seat: {seats.seating}</span>
-															<p>Price: ${seats.value} MXN</p>
-														</Card>
-													</>
-												))}
-											</div>
-
-											<div>
-												Final: <b>${this.state.totalPrice} MXN</b>
-											</div>
-
-											<div>
-												<BuyTickets />
-											</div>
-										</>)}
+									<Layout.Col span="18">
+			
+										<h1 className="mb-mr pt-2">{event.name}</h1>
+										<h2 className="mb-mr">{event.description}</h2>
+										<Icon name="date" className="date" />{event.validUntil}
+										<div>
+											<span className="pt-1">By: {event.owner}</span>
+										</div>
+										<Layout.Row style={{ marginTop: "1%" }}>
+											<Layout.Col span="16">
+												<Transfer
+													style={{padding: 0}}
+													filterable
+													filterMethod={this._filterMethod}
+													filterPlaceholder="Categorias"
+													value={selectedTicket}
+													onChange={this._handleChange}
+													data={this.state.data}
+													titles={['Disponibles', 'Seleccionados']}
+			
+													footerFormat={{
+														//eslint-disable-next-line
+														noChecked: '${total}',
+														//eslint-disable-next-line
+														hasChecked: '${checked}/${total}'
+													}}>
+			
+												</Transfer>
+											</Layout.Col>
+											<Layout.Col span="8">
+			
+											</Layout.Col>
+										</Layout.Row>
+									</Layout.Col>
+									<Layout.Col span="6">
+										<Card style={{ marginTop: "1%" }}>
+											<Layout.Row style={{alignItems: "center"}}>
+												<Layout.Col span="24">
+													{selectedTicket.length === 0 ? (
+														<h3>No Tickets Selected</h3>
+													) : (<>
+														<h3>{selectedTicket.length} x Tickets Selected</h3>
+			
+														<div>
+															{this.state.seats.map(seats => (
+																<>
+																	<Card style={{ marginTop: "1%", marginBottom: "1%" }}>
+																		<h2>{seats.category}</h2>
+																		<span>Seat: {seats.seating}</span>
+																		<p>Price: ${seats.value} MXN</p>
+																	</Card>
+																</>
+															))}
+														</div>
+			
+														<div style={{ marginTop: "1%", marginBottom: "2%", alignSelf: "center" }}>
+															Final: <b>${this.state.totalPrice} MXN</b>
+														</div>
+			
+														<div style={{ marginTop: "1%", marginBottom: "1%",alignSelf: "center"}}>
+															<BuyTickets tickets={this.state.seats} user={user}/>
+														</div>
+													</>)}
+												</Layout.Col>
+											</Layout.Row>
+										</Card>
 									</Layout.Col>
 								</Layout.Row>
 							</Card>
-						</Layout.Col>
-					</Layout.Row>
-				</Card>
-			</>
+						</>
+					)
+				}}
+			</UserContext.Consumer>
 		)
+
+		
 	}
 }
 
